fb789522f4c5ee57cdcbfc4010907465c3411491
LaMont Jones <lamontjones@google.com>
2025-09-19 02:21:15 +0000

    Update siso to 760362be9f7ce8728e2538cc02a84aa83c6b05e9 (v1.4.6)
    This adds:
    - Script to generate Android.bp files
    - `siso proxy` support
    
    Also update update_siso_prebuilts.sh to take the git hash or a tag and
    update to that.
    
    Bug: b/439871372
    Bug: b/445277738
    Test: manual
    Change-Id: I81d8fab90cbeb67fd1dd2f5030b3a169ce92925d

--- update_siso_prebuilts.sh
+++ update_siso_prebuilts.sh
@@ -8,7 +8,19 @@ if ! git diff HEAD --quiet; then
     exit 1
 fi
 
-readonly tmpdir=$(mktemp -d)
+version=${1:-latest}
+# Turn the argument given into a git revision.
+hash=$(cipd describe build/siso/linux-amd64 -version ${version} | sed -n '/^  git_revision:/s/.*revision://p')
+if [[ -z "${hash}" ]]; then
+  hash=$(cipd describe build/siso/linux-amd64 -version git_revision:${version} | sed -n '/^  git_revision:/s/.*revision://p')
+fi
+if [[ -z "${hash}" ]]; then
+  echo "Could not find revision '${version}'" >& 2
+  exit 1
+fi
+
+# Update siso.ensure
+sed -i "/build\/siso.*git_revision:[0-9a-f]*$/s/\\(git_revision\\):[0-9a-f]*\$/\1:${hash}/" siso.ensure
 
 cipd ensure-file-resolve -ensure-file siso.ensure
 # This avoids cipd creating `.cpid` and using symlinks.
